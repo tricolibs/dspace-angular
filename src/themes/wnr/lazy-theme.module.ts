d4e6397a05de9df0924e916d2df650127dfe7ffb
Howard Ding <hding2@brynmawr.edu>
2026-03-24 15:11:16 -0400

    reinstate wnr theme

---
+++ src/themes/wnr/lazy-theme.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
+import { StoreModule } from '@ngrx/store';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
+import { RootModule } from '../../app/root.module';
+import { FileSectionComponent } from './app/item-page/simple/field-components/file-section/file-section.component';
+import { ItemPageComponent } from './app/item-page/simple/item-page.component';
+import { FullItemPageComponent } from './app/item-page/full/full-item-page.component';
+
+const DECLARATIONS = [
+  FileSectionComponent,
+  ItemPageComponent,
+  FullItemPageComponent,
+];
+
+@NgModule({
+  imports: [
+    RootModule,
+    CommonModule,
+    DragDropModule,
+    FormsModule,
+    HttpClientModule,
+    NgbModule,
+    RouterModule,
+    ScrollToModule,
+    StoreModule,
+    StoreRouterConnectingModule,
+    TranslateModule,
+    FormsModule,
+    ...DECLARATIONS,
+  ],
+  exports: [],
+})
+
+  /**
+   * This module serves as an index for all the components in this theme.
+   * It should import all other modules, so the compiler knows where to find any components referenced
+   * from a component in this theme
+   * It is purposefully not exported, it should never be imported anywhere else, its only purpose is
+   * to give lazily loaded components a context in which they can be compiled successfully
+   */
+class LazyThemeModule {
+}
